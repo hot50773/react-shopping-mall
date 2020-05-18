16f3b7ab60012c1b1be65806d972b0475e31f4ea
Ｍindy <hot50773@gmail.com>
2020-05-18 18:56:26 +0800

    Update - add component 'product details' and test it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import MyHeader from './MyHeader'
 import MyBody from './MyBody'
+import ProductDetails from './components/ProductDetails'
 
 function App () {
+  const data = {
+    imgSrc: 'https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=951&q=80',
+    name: '灰色質感羊毛上衣 - 女',
+    price: '$1299',
+    storage: [
+      { size: 'S', storageNum: 2 },
+      { size: 'M', storageNum: 2 },
+      { size: 'L', storageNum: 2 }
+    ]
+  }
   return (
     <div>
       <MyHeader />
-      <MyBody />
+      <ProductDetails name={data.name} imgSrc={data.imgSrc} price={data.price} storage={data.storage} />
+      {/* <MyBody /> */}
     </div>
   )
 }
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input, CustomInput } from 'reactstrap'
+
+// TODO: Router https://pjchender.blogspot.com/2018/11/react-react-router-dynamic-breadcrumb.html
+class ProductDetails extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  renderSizeOption ({ size = '', storageNum } = {}) {
+    if (typeof storageNum === 'number' && storageNum === 0) {
+      console.log(`storageNumber of size ${size} is 0`)
+      return
+    }
+    // TODO:Typechecking With PropTypes -> https://zh-hant.reactjs.org/docs/typechecking-with-proptypes.html
+
+    // 語法檢查: storageNum
+    if (storageNum === '' || storageNum === null || typeof storageNum === 'undefined') throw new Error('function input "storageNum" should not be empty or undefined')
+    if (typeof storageNum !== 'number') throw new Error('variable "storageNum" should be a number')
+    if (storageNum < 0) throw new Error('storageNumber of size should not be empty')
+
+    // 語法檢查: size
+    if (size === '' || size === null || typeof size === 'undefined') throw new Error('function input "size" should not be empty or undefined')
+    if (typeof size !== 'string') throw new Error('variable "size" should be a string')
+
+    // 渲染元素
+    // if (storageNum > 0) return (<option key={size}>{size}</option>)
+
+    if (storageNum > 0) {
+      return (
+        <CustomInput id={`inpRadio${size}`} key={`inpRadio${size}`} name='inpRadioSize' type='radio' label={size} inline />
+      )
+    }
+  }
+
+  renderIntegerOptions ({ min, max } = {}) {
+    // 用迭代的方式生成元素: https://zh-hant.reactjs.org/docs/lists-and-keys.html
+
+    const options = []
+    for (let num = Math.trunc(min); num <= Math.trunc(max); num++) {
+      // 注意：Each child in a list should have a unique "key" prop.
+      options.push(<option key={num}>{num}</option>)
+    }
+
+    return options
+  }
+
+  render () {
+    return (
+      <Container className='p-5'>
+        <Row>
+          <Col sm='0' md='1' />
+          <Col sm='6' md='4'>
+            <img src={this.props.imgSrc} alt={this.props.name} className='w-100' />
+          </Col>
+          <Col sm='6' md='6'>
+            <h3>{this.props.name}</h3>
+            <br />
+            <h3 className='text-danger'>{this.props.price}</h3>
+            <br />
+            <Form>
+              <FormGroup>
+                <Label>選擇尺寸</Label>
+                <div>
+                  {
+                    this.props.storage.map(item => {
+                      if (item.size === undefined) console.log('"item.size" is undefined')
+                      if (item.storageNum === undefined) console.log('"item.storageNum" is undefined')
+
+                      return this.renderSizeOption({ size: item.size, storageNum: item.storageNum })
+                    })
+                  }
+                </div>
+              </FormGroup>
+
+              <FormGroup>
+                <Label>數量</Label>
+                <Input type='select' name='inpSelectNumber' id='inpSelectNumber'>
+                  {this.renderIntegerOptions({ min: 1, max: 10 })}
+                </Input>
+              </FormGroup>
+            </Form>
+            <br />
+            <Button color='primary'>加入購物車</Button>
+          </Col>
+          <Col sm='0' md='1' />
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default ProductDetails
