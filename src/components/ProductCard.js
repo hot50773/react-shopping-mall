590a52195b0861b15a91c9c7ba33ba2863b17401
Ｍindy <hot50773@gmail.com>
2020-06-02 16:45:48 +0800

    Update Product Card: modify cover image style and add dynamic data src input

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,12 +3,12 @@ import './ProductCard.css'
 
 function ProductCard (props) {
   return (
-    <div class='product-card'>
-      <a class='product-card__img-wrapper' href='/detail/product-id'>
-        <img class='product-card__img' src='https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=951&q=80' alt='灰色質感羊毛上衣 - 女' />
+    <div className='product-card'>
+      <a className='product-card__img-wrapper' href={`/detail/${props.id}`}>
+        <div className='product-card__img' style={{ backgroundImage: `url("${props.imgSrc}")` }} alt={props.name} />
       </a>
-      <div class='product-card__name'>灰色質感羊毛上衣 - 女</div>
-      <div class='product-card__price'>$1299</div>
+      <div className='product-card__name'>{props.name}</div>
+      <div className='product-card__price'>{props.price}</div>
     </div>
   )
 }
